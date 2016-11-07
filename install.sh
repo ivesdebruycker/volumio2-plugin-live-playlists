de475a2206949a08fcad96fc8cb0db67447c2617
Ives De Bruycker <ives.debruycker@gmail.com>
2016-11-07 13:37:46 +0000

    Correct permission of config file Install NodeJS dependencies

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 echo "Initializing config"
 mkdir /data/configuration/music_service/live_playlists
 cp /data/plugins/music_service/live_playlists/config.json /data/configuration/music_service/live_playlists/config.json
+chown volumio:volumio /data/configuration/music_service/live_playlists/config.json
+
+echo "Install NodeJS dependencies"
+( cd /data/plugins/music_service/live_playlists && npm install )
 
 echo "plugin install end"
